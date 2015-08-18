f975f84b641fbf6f5dd18e5616027f64dff8845e
oleg-l <oleg.lekshin@gmail.com>
2015-08-18 03:09:44 +0300

    some errors fix

--- org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/base/json/parser/JsonArray.java
+++ org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/base/json/parser/JsonArray.java
@@ -171,7 +171,7 @@ public class JsonArray extends OBJECT {
         return new string(array.toString());
     }
 
-    public static JsonArray.CLASS<JsonArray> getJsonObject(org.zenframework.z8.server.json.parser.JsonArray json) {
+    public static JsonArray.CLASS<JsonArray> getJsonArray(org.zenframework.z8.server.json.parser.JsonArray json) {
         JsonArray.CLASS<JsonArray> jsonArray = new JsonArray.CLASS<JsonArray>(null);
         jsonArray.get().set(json);
         return jsonArray;
