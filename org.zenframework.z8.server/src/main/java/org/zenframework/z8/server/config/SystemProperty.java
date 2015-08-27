f34d495df35f4035a3d71592640126e727fdc0c1
zen-maks <derevyanko@VDI-DEREVYANKO.maindomain.ivk.ru>
2015-08-27 17:38:07 +0300

    Remove unused properties

--- org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/config/SystemProperty.java
+++ org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/config/SystemProperty.java
@@ -1,8 +1,9 @@
 package org.zenframework.z8.server.config;
 
 public class SystemProperty {
+
     static public String ConfigFilePath = "org.zenframework.z8.project";
 
     static public String RAAS = "java.security.auth.login.config";
-    static public String BIRT_HOME = "BIRT_HOME";
+
 }
