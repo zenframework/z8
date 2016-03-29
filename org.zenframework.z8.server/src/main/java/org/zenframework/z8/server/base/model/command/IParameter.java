a60798433e2ed05aeae01fb2b967e095e9ed58b0
zen-maks <derevyanko@VDI-DEREVYANKO.maindomain.ivk.ru>
2016-03-29 13:29:08 +0300

    jsonWriter

--- org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/base/model/command/IParameter.java
+++ org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/base/model/command/IParameter.java
@@ -1,7 +1,7 @@
 package org.zenframework.z8.server.base.model.command;
 
 import org.zenframework.z8.server.db.FieldType;
-import org.zenframework.z8.server.json.parser.JsonObject;
+import org.zenframework.z8.server.json.JsonWriter;
 import org.zenframework.z8.server.request.INamedObject;
 
 public interface IParameter extends INamedObject {
@@ -13,5 +13,5 @@ public interface IParameter extends INamedObject {
 
     public void parse(String value);
 
-    public void write(JsonObject writer);
+    public void write(JsonWriter writer);
 }
