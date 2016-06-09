fbd108f41dc530531782447569baf38403f0c165
Oleg S. Lekshin <oleg.lekshin@gmail.com>
2016-06-09 16:55:54 +0300

    ErrorUtils.getMessage() message detalization

--- org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/utils/ErrorUtils.java
+++ org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/utils/ErrorUtils.java
@@ -1,25 +1,26 @@
 package org.zenframework.z8.server.utils;
 
 public class ErrorUtils {
-    static public String getMessage(Throwable throwable) {
-        Throwable exception = throwable;
-        while(exception.getCause() != null)
-            exception = exception.getCause();
-        
-        String message = exception.getMessage();
 
-        if(message == null || message.isEmpty())
-            return "Internal server error.";
-        
-        return message;
-    }
+	static public String getMessage(Throwable e) {
+		StringBuilder message = new StringBuilder();
+		for (; e != null && e.getCause() != e; e = e.getCause()) {
+			String s = e.getMessage();
+			if (s != null && !s.isEmpty())
+				message.append(s).append(" <-- ");
+		}
+		if (message.length() > 4)
+			message.setLength(message.length() - 5);
+		return message.length() == 0 ? "Internal server error." : message.toString();
+	}
 
-    static public String getStackTrace(Throwable throwable) {
-        StringBuilder stackTrace = new StringBuilder();
+	static public String getStackTrace(Throwable throwable) {
+		StringBuilder stackTrace = new StringBuilder();
 
-        for(StackTraceElement element : throwable.getStackTrace())
-            stackTrace.append("\t" + element.toString() + "\r\n");
-        
-        return stackTrace.toString();
-    }
+		for (StackTraceElement element : throwable.getStackTrace())
+			stackTrace.append("\t" + element.toString() + "\r\n");
+
+		return stackTrace.toString();
+	}
+
 }
