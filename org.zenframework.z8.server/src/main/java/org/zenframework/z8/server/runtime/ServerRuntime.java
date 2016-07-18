fe44ae6b76d6a7031bbdfd99610380e636e06151
zen-maks <derevyanko@vdi-auto-02.maindomain.ivk.ru>
2016-07-18 22:32:24 +0300

    ok

--- org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/runtime/ServerRuntime.java
+++ org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/runtime/ServerRuntime.java
@@ -24,29 +24,7 @@ public class ServerRuntime extends AbstractRuntime {
 	public static final Property PreserveMessagesQueueProperty = new Property("8D9C727A-34FC-4DCD-9AB0-5A2AF8E676E0",
 			"z8.transport.preserveExportMessages", "false",
 			"Сохранять локальную очередь экспортируемых сообщений (true / false)");
-//	public static final Property EnableProtocolsProperty = new Property("222A95B9-05BC-4AF3-8425-323D8B1A1B73",
-//			"z8.transport.enableProtocols", "", "Список (через ',') протоколов, инициализируемых при старте");
-//	public static final Property ExportRecordsMaxProperty = new Property("FAB69475-114C-4C39-8401-A03C42608BBE",
-//			"z8.transport.exportRecordsMax", "1000", "Максимальное количество записей в сообщении экспорта");
-//	public static final Property RecordsSortingModeProperty = new Property("FA9D7410-050B-43BB-9F7A-B151F5BCD091",
-//			"z8.transport.recordsSortingMode", "always",
-//			"Режим сортировки записей при импорте/экспорте (on_export, on_import, always)");
-//	public static final Property InactiveRouteTimeoutProperty = new Property("B7CA8C2A-430C-44BC-8DFA-FEF11A7B115B",
-//			"z8.transport.inactiveRouteTimeout", "30",
-//			"Время простоя маршрута, по которому не удалось отправить сообщение, минуты");
-//	public static final Property FileFolderProperty = new Property("3D524DE7-0AF3-40FE-8FF7-C4A073D1F834",
-//			"z8.transport.file.folder", "C:/z8/transport", "Каталог для обмена по протоколу file");
-/*	public static final Property JmsConnectionFactoryProperty = new Property("5CACA325-B353-43C0-917C-B9A20C21C64E",
-			"z8.transport.jms.connectionFactory", "org.apache.activemq.jndi.ActiveMQInitialContextFactory",
-			"Класс-фабрика для получения JMS-соединений");
-	public static final Property JmsConnectionUrlProperty = new Property("7CAF19ED-71A5-480F-A2D3-95C5B22B4CA6",
-			"z8.transport.jms.connectionUrl", "tcp://localhost:61616?trace=false&soTimeout=60000",
-			"URL для подключения к JMS-серверу");
-	public static final Property JmsModeProperty = new Property("543A3B9F-04F0-4697-AEBA-ABBF5693865B",
-			"z8.transport.jms.mode", "object", "Режим передачи сообщений через JMS (object, stream). По умолчанию - object");
-	public static final Property WsEndpointProperty = new Property("E90D5A9C-6C4A-48A2-BA3C-34E2F69DEF11",
-			"z8.transport.ws.endpoint", "http://localhost:9898/transport", "URL транспортного web-сервиса");
-*/
+
 	public ServerRuntime() {
 		addTable(new Users.CLASS<Users>(null));
 		addTable(new SecurityGroups.CLASS<SecurityGroups>(null));
@@ -72,15 +50,6 @@ public class ServerRuntime extends AbstractRuntime {
 
 		addProperty(DbSchemeControlSumProperty);
 		addProperty(PreserveMessagesQueueProperty);
-//		addProperty(EnableProtocolsProperty);
-//		addProperty(ExportRecordsMaxProperty);
-//		addProperty(RecordsSortingModeProperty);
-//		addProperty(InactiveRouteTimeoutProperty);
-//		addProperty(FileFolderProperty);
-//		addProperty(JmsConnectionFactoryProperty);
-//		addProperty(JmsConnectionUrlProperty);
-//		addProperty(JmsModeProperty);
-//		addProperty(WsEndpointProperty);
 	}
 
 }
