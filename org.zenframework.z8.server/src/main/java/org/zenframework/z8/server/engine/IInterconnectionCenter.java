7b3ca56f4f4521762d1ec5aa63c452d97a3f23e0
zen-maks <derevyanko@vdi-auto-02.maindomain.ivk.ru>
2016-07-19 18:29:41 +0300

    ok

--- org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/engine/IInterconnectionCenter.java
+++ org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/engine/IInterconnectionCenter.java
@@ -2,6 +2,11 @@ package org.zenframework.z8.server.engine;
 
 import java.rmi.RemoteException;
 
+import org.zenframework.z8.server.types.file;
+
 public interface IInterconnectionCenter extends IHubServer {
 	IApplicationServer connect(String domain) throws RemoteException;
+	
+	public boolean accept(IApplicationServer server, Object data) throws RemoteException;
+	public boolean hasFile(IApplicationServer server, file file) throws RemoteException;
 }
