048f4d366c2bde36716bac31c037318df98adce8
zen-maks <derevyanko@vdi-derevyank-8.maindomain.ivk.ru>
2016-07-25 02:18:22 +0300

    ok

--- org.zenframework.z8.pde/src/main/java/org/zenframework/z8/pde/build/Z8ProjectBuilder.java
+++ org.zenframework.z8.pde/src/main/java/org/zenframework/z8/pde/build/Z8ProjectBuilder.java
@@ -22,64 +22,64 @@ import org.zenframework.z8.pde.Plugin;
 import org.zenframework.z8.pde.jobs.Z8Job;
 
 public class Z8ProjectBuilder extends IncrementalProjectBuilder {
-    final static public String Id = "org.zenframework.z8.pde.ProjectBuilder";
+	final static public String Id = "org.zenframework.z8.pde.ProjectBuilder";
 
-    final static public QualifiedName PrelaunchBuild = new QualifiedName("org.zenframework.z8.pde", "PrelaunchBuild");
+	final static public QualifiedName PrelaunchBuild = new QualifiedName("org.zenframework.z8.pde", "PrelaunchBuild");
 
-    public Z8ProjectBuilder() {}
+	public Z8ProjectBuilder() {
+	}
 
-    protected void abort(String message, Throwable exception, int code) throws CoreException {
-        throw new CoreException(new Status(IStatus.ERROR, Plugin.getUniqueIdentifier(), code, message, exception));
-    }
+	protected void abort(String message, Throwable exception, int code) throws CoreException {
+		throw new CoreException(new Status(IStatus.ERROR, Plugin.getUniqueIdentifier(), code, message, exception));
+	}
 
-    @Override
-    @SuppressWarnings("rawtypes")
-    protected IProject[] build(int kind, Map args, IProgressMonitor monitor) throws CoreException {
-        Project project = Workspace.getInstance().getProject(getProject());
+	@Override
+	@SuppressWarnings("rawtypes")
+	protected IProject[] build(int kind, Map args, IProgressMonitor monitor) throws CoreException {
+		IProject iProject = getProject();
+		Project project = Workspace.getInstance().getProject(iProject);
 
-        final boolean[] saved = { true };
+		final boolean[] saved = { true };
 
-        if(getProject().getSessionProperty(PrelaunchBuild) != null) {
-            Display.getDefault().syncExec(new Runnable() {
-                @Override
-                public void run() {
-                    saved[0] = PlatformUI.getWorkbench().saveAllEditors(true);
-                }
-            });
-        }
+		if(getProject().getSessionProperty(PrelaunchBuild) != null) {
+			Display.getDefault().syncExec(new Runnable() {
+				@Override
+				public void run() {
+					saved[0] = PlatformUI.getWorkbench().saveAllEditors(true);
+				}
+			});
+		}
 
-        if(saved[0]) {
-            BuildMessageConsumer consumer = new BuildMessageConsumer();
+		if(saved[0]) {
+			BuildMessageConsumer consumer = new BuildMessageConsumer();
 
-            try {
-                project.build(consumer, monitor);
-            }
-            catch(Throwable e) {
-                abort("Internal compiler error", e, IResourceStatus.BUILD_FAILED);
-            }
-            finally {
-                forgetLastBuiltState();
-            }
-        }
+			try {
+				project.build(consumer, monitor);
+				iProject.refreshLocal(IResource.DEPTH_INFINITE, monitor);
+			} catch(Throwable e) {
+				abort("Internal compiler error", e, IResourceStatus.BUILD_FAILED);
+			} finally {
+				forgetLastBuiltState();
+			}
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    @Override
-    protected void clean(IProgressMonitor monitor) throws CoreException {
-        IProject project = getProject();
+	@Override
+	protected void clean(IProgressMonitor monitor) throws CoreException {
+		IProject project = getProject();
 
-        try {
-            project.deleteMarkers(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE);
-            forgetLastBuiltState();
-        }
-        catch(Throwable e) {
-            Plugin.log(e);
-        }
-    }
+		try {
+			project.deleteMarkers(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE);
+			forgetLastBuiltState();
+		} catch(Throwable e) {
+			Plugin.log(e);
+		}
+	}
 
-    public static void focusOnProblemsView() {
-        Z8Job job = new Z8Job(Z8Job.JobType.jtShowView, IPageLayout.ID_PROBLEM_VIEW);
-        job.startJob();
-    }
+	public static void focusOnProblemsView() {
+		Z8Job job = new Z8Job(Z8Job.JobType.jtShowView, IPageLayout.ID_PROBLEM_VIEW);
+		job.startJob();
+	}
 }
