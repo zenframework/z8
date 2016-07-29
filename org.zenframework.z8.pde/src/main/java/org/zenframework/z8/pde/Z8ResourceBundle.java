6dda966d5f2c0fcdffb3d3106de3c648fbf09afb
zen-maks <derevyanko@vdi-derevyank-8.maindomain.ivk.ru>
2016-07-29 16:31:48 +0300

    ok

--- org.zenframework.z8.pde/src/main/java/org/zenframework/z8/pde/Z8ResourceBundle.java
+++ org.zenframework.z8.pde/src/main/java/org/zenframework/z8/pde/Z8ResourceBundle.java
@@ -4,16 +4,21 @@ import java.util.ListResourceBundle;
 
 public class Z8ResourceBundle extends ListResourceBundle {
 
-    final static Object[][] contents = new Object[][] { { "ContentAssistProposal.label", "Автозамена" },
-            { "ContentAssistTip.label", "Автоподсказка" }, { "OpenDeclaration.label", "Перейти к определению" },
-            { "OpenTablesEditor.label", "Схема данных" }, { "FindEntryPointPaths.label", "Пути от точек входа" },
-            { "OpenTextHierarchy.label", "Открыть иерархию типов" }, { "OrganizeImports.label", "Организовать import'ы" },
-            { "OpenFormsEditor.label", "Открыть представление" },
-            { "CreateGUID.label", "Вставить уникальный идентификатор" } };
+	final static Object[][] contents = new Object[][] { 
+		{ "ContentAssistProposal.label", "Автозамена" }, 
+		{ "ContentAssistTip.label", "Автоподсказка" }, 
+		{ "OpenDeclaration.label", "Перейти к определению" }, 
+		{ "OpenTablesEditor.label", "Схема данных" },
+		{ "FindEntryPointPaths.label", "Пути от точек входа" }, 
+		{ "OpenTextHierarchy.label", "Открыть иерархию типов" }, 
+		{ "OrganizeImports.label", "Организовать import'ы" }, 
+		{ "OpenFormsEditor.label", "Открыть представление" },
+		{ "CreateGUID.label", "Вставить уникальный идентификатор" }
+	};
 
-    @Override
-    protected Object[][] getContents() {
-        return contents;
-    }
+	@Override
+	protected Object[][] getContents() {
+		return contents;
+	}
 
 }
