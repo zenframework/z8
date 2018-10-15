b518e7dbd406cea3fd670a035a7082f340fac86d
Aleksandr Andriushchenko <andriushchenko@ivk.ru>
2018-10-15 17:41:05 +0300

    PostgreSQL: sql_string.isNumeric()

--- org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/db/sql/functions/conversion/IsIntString.java
+++ org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/db/sql/functions/conversion/IsIntString.java
@@ -1,50 +1,16 @@
 package org.zenframework.z8.server.db.sql.functions.conversion;
 
-import java.util.Collection;
-
-import org.zenframework.z8.server.base.table.value.IField;
-import org.zenframework.z8.server.db.DatabaseVendor;
-import org.zenframework.z8.server.db.FieldType;
-import org.zenframework.z8.server.db.sql.FormatOptions;
 import org.zenframework.z8.server.db.sql.SqlConst;
-import org.zenframework.z8.server.db.sql.SqlStringToken;
 import org.zenframework.z8.server.db.sql.SqlToken;
-import org.zenframework.z8.server.db.sql.functions.CaseToken;
-import org.zenframework.z8.server.db.sql.functions.string.RegIndexOf;
-import org.zenframework.z8.server.types.integer;
-import org.zenframework.z8.server.types.sql.sql_string;
+import org.zenframework.z8.server.db.sql.functions.string.RegLike;
+import org.zenframework.z8.server.types.string;
 
-public class IsIntString extends SqlToken {
-	private SqlToken string;
+public class IsIntString extends RegLike {
+
+	private static final String PATTERN = "^[0-9]+$";
 
 	public IsIntString(SqlToken string) {
-		this.string = string;
+		super(string, new SqlConst(new string(PATTERN)));
 	}
 
-	@Override
-	public void collectFields(Collection<IField> fields) {
-		string.collectFields(fields);
-	}
-
-	@Override
-	public String format(DatabaseVendor vendor, FormatOptions options, boolean logicalContext) {
-		CaseToken CaseToken = new CaseToken();
-		String token = new RegIndexOf(string, new sql_string(pattern())).format(vendor, options, logicalContext) + " < 0";
-		CaseToken.addWhen(new SqlStringToken(token, FieldType.Boolean), new SqlConst(new integer(1)));
-		CaseToken.setElse(new SqlConst(new integer(0)));
-		String result = CaseToken.format(vendor, options);
-		if(logicalContext) {
-			result = "(" + result + ") = 1"; // Это случай использования в Where
-		}
-		return result;
-	}
-
-	@Override
-	public FieldType type() {
-		return FieldType.Boolean;
-	}
-
-	protected String pattern() {
-		return "%[^0-9]%";
-	}
 }
--- org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/db/sql/functions/conversion/IsNumericString.java
+++ org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/db/sql/functions/conversion/IsNumericString.java
@@ -1,14 +1,16 @@
 package org.zenframework.z8.server.db.sql.functions.conversion;
 
+import org.zenframework.z8.server.db.sql.SqlConst;
 import org.zenframework.z8.server.db.sql.SqlToken;
+import org.zenframework.z8.server.db.sql.functions.string.RegLike;
+import org.zenframework.z8.server.types.string;
 
-public class IsNumericString extends IsIntString {
-	public IsNumericString(SqlToken numeric) {
-		super(numeric);
+public class IsNumericString extends RegLike {
+
+	private static final String PATTERN = "^[0-9\\.]+$";
+
+	public IsNumericString(SqlToken string) {
+		super(string, new SqlConst(new string(PATTERN)));
 	}
 
-	@Override
-	protected String pattern() {
-		return "%[^0-9|.]%";
-	}
 }
