0cc57a68e386708d69dab883ee0ce71855274da1
zen-maks <x120573@gmail.com>
2018-12-17 15:58:25 +0300

    ok

--- org.zenframework.z8.js/src/js/application/viewport/PopupMessages.js
+++ org.zenframework.z8.js/src/js/application/viewport/PopupMessages.js
@@ -2,24 +2,33 @@ Z8.define('Z8.viewport.PopupMessages', {
 	extend: 'Z8.Container',
 
 	cls: 'messages popup',
+	stacked: true,
+	autoHide: true,
 
 	show: function(message) {
-		var messages = Array.isArray(message) ? message : [message];
-
 		var items = this.items;
 		var count = items.length;
-		var news = messages.length;
 
-		if(count + news > 5) {
-			for(var i = count - 1; i >= Math.max(5 - news, 0); i--)
-				items[i].remove();
+		if(this.stacked) {
+			var messages = Array.isArray(message) ? message : [message];
+
+			var news = messages.length;
+
+			if(count + news > 5) {
+				for(var i = count - 1; i >= Math.max(5 - news, 0); i--)
+					items[i].remove();
+			}
+
+			var items = [];
+			for(var i = Math.max(0, news - 5); i < news; i++)
+				items.insert(new Z8.viewport.PopupMessage(messages[i]), 0);
+			this.add(items, 0);
+		} else {
+			if(count > 0)
+				items[count - 1].remove();
+			if(message != null)
+				this.add(new Z8.viewport.PopupMessage(message));
 		}
-
-		var items = [];
-		for(var i = Math.max(0, news - 5); i < news; i++)
-			items.insert(new Z8.viewport.PopupMessage(messages[i]), 0);
-
-		this.add(items, 0);
 	}
 });
 
@@ -45,7 +54,8 @@ Z8.define('Z8.viewport.PopupMessage', {
 		this.callParent();
 
 		DOM.on(this, 'click', this.hide, this);
-		new Z8.util.DelayedTask().delay(this.messageDelay, this.hide, this);
+		if(this.container.autoHide)
+			new Z8.util.DelayedTask().delay(this.messageDelay, this.hide, this);
 	},
 
 	onDestroy: function() {
