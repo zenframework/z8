f8986a686681e529827bfda6bae73c2ee5b836b2
Oleg S. Lekshin <oleg.lekshin@gmail.com>
2019-01-11 14:02:17 +0300

    sum(geometry) -> ST_Union

--- org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/db/sql/functions/Sum.java
+++ org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/db/sql/functions/Sum.java
@@ -1,9 +1,14 @@
 package org.zenframework.z8.server.db.sql.functions;
 
+import org.zenframework.z8.server.db.FieldType;
 import org.zenframework.z8.server.db.sql.SqlToken;
 
 public class Sum extends Window {
 	public Sum(SqlToken token) {
-		super(token, "sum");
+		super(token, getSumFunction(token));
+	}
+
+	protected static String getSumFunction(SqlToken token) {
+		return token.type() == FieldType.Geometry ? "ST_Union" : "sum";
 	}
 }
