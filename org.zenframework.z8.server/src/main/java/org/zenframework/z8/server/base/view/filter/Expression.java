76d7ed879ddef5ef3bb874d79f287c421674001b
Oleg S. Lekshin <oleg.lekshin@gmail.com>
2019-02-07 16:40:55 +0300

    Expression revert

--- org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/base/view/filter/Expression.java
+++ org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/base/view/filter/Expression.java
@@ -1,5 +1,8 @@
 package org.zenframework.z8.server.base.view.filter;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.zenframework.z8.server.base.query.Query;
 import org.zenframework.z8.server.base.table.value.Field;
 import org.zenframework.z8.server.base.table.value.GeometryExpression;
@@ -18,6 +21,8 @@ import org.zenframework.z8.server.db.sql.functions.conversion.GuidToString;
 import org.zenframework.z8.server.db.sql.functions.string.Like;
 import org.zenframework.z8.server.db.sql.functions.string.Lower;
 import org.zenframework.z8.server.json.Json;
+import org.zenframework.z8.server.json.parser.JsonArray;
+import org.zenframework.z8.server.json.parser.JsonException;
 import org.zenframework.z8.server.json.parser.JsonObject;
 import org.zenframework.z8.server.types.bool;
 import org.zenframework.z8.server.types.date;
@@ -25,6 +30,8 @@ import org.zenframework.z8.server.types.decimal;
 import org.zenframework.z8.server.types.geometry;
 import org.zenframework.z8.server.types.guid;
 import org.zenframework.z8.server.types.integer;
+import org.zenframework.z8.server.types.primary;
+import org.zenframework.z8.server.types.string;
 import org.zenframework.z8.server.types.sql.sql_bool;
 import org.zenframework.z8.server.types.sql.sql_date;
 import org.zenframework.z8.server.types.sql.sql_geometry;
@@ -33,17 +40,62 @@ import org.zenframework.z8.server.types.sql.sql_string;
 public class Expression implements IFilter {
 	private Field field;
 	private Operation operation;
-	private String value;
+	private String[] values;
 
 	public Expression(JsonObject expression, Query query) {
 		String field = expression.has(Json.property) ? expression.getString(Json.property) : null;
-		String value = expression.has(Json.value) ? expression.getString(Json.value) : null;
+		String values = expression.has(Json.value) ? expression.getString(Json.value) : null;
 		String operator = expression.has(Json.operator) ? expression.getString(Json.operator) : null;
 
 		this.operation = operator != null ? Operation.fromString(operator) : Operation.Eq;
 
 		this.field = field != null ? query.findFieldById(field) : null;
-		this.value = value;
+		this.values = parseValues(values);
+	}
+
+	private String[] parseValues(String jsonData) {
+		Collection<String> result = new ArrayList<String>();
+
+		if(jsonData == null || jsonData.isEmpty())
+			return new String[0];
+
+		char startChar = jsonData.charAt(0);
+
+		if(startChar == '[') {
+			try {
+				JsonArray values = new JsonArray(jsonData);
+				for(Object value : values)
+					result.add((String)value);
+			} catch (JsonException e) {
+				result.add(jsonData);
+			}
+		} else
+			result.add(jsonData);
+
+		return result.toArray(new String[0]);
+	}
+
+	private Collection<primary> getValues(FieldType type) {
+		Collection<primary> result = new ArrayList<primary>();
+
+		for(String value : values) {
+			if(type == FieldType.Date || type == FieldType.Datetime)
+				result.add(new date(value));
+			else if(type == FieldType.Guid)
+				result.add(new guid(value));
+			else if(type == FieldType.Decimal)
+				result.add(new decimal(value));
+			else if(type == FieldType.Integer)
+				result.add(new integer(value));
+			else if(type == FieldType.Boolean)
+				result.add(new bool(value));
+			else if(type == FieldType.String || type == FieldType.Text || type == FieldType.Attachments || type == FieldType.File)
+				result.add(new string(value));
+			else
+				throw new UnsupportedOperationException();
+		}
+
+		return result;
 	}
 
 	public SqlToken where() {
@@ -51,6 +103,20 @@ public class Expression implements IFilter {
 			return null;
 
 		FieldType type = field.type();
+
+		if(values.length > 1) {
+			if(operation != Operation.Eq && operation != Operation.NotEq || type == FieldType.Date || type == FieldType.Datetime)
+				throw new UnsupportedOperationException();
+
+			SqlToken result = field.inVector(getValues(type));
+
+			if(operation == Operation.NotEq)
+				result = new Unary(Operation.Not, result);
+
+			return result;
+		}
+
+		String value = values.length != 0 ? values[0] : null;
 
 		switch(type) {
 		case Boolean:
@@ -131,7 +197,7 @@ public class Expression implements IFilter {
 		case String:
 		case Text:
 		case Attachments:
-		case File: {
+		case File:
 			SqlToken field = new SqlField(this.field);
 
 			if(type == FieldType.Guid) {
@@ -197,14 +263,13 @@ public class Expression implements IFilter {
 			default:
 				throw new UnsupportedOperationException();
 			}
-		}
 		case Geometry:
-			return new Intersects(field, new sql_geometry(geometry.fromGeoJson(value, srs(field))));
+			return new Intersects(this.field, new sql_geometry(geometry.fromGeoJson(value, srs(this.field))));
 		default:
 			throw new UnsupportedOperationException();
 		}
 	}
-
+	
 	static private int srs(Field field) {
 		if (field instanceof GeometryField)
 			return ((GeometryField) field).srs.getInt();
@@ -212,4 +277,5 @@ public class Expression implements IFilter {
 			return ((GeometryExpression) field).srs.getInt();
 		throw new UnsupportedOperationException();
 	}
+
 }
