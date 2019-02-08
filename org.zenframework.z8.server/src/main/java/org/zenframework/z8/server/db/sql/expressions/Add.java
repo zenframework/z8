ec2f9c7ccad08dad01eb9dedbbf1e4f549f29c3c
zen-maks <x120573@gmail.com>
2019-02-08 20:46:28 +0300

    ok

--- org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/db/sql/expressions/Add.java
+++ org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/db/sql/expressions/Add.java
@@ -5,8 +5,6 @@ import org.zenframework.z8.server.db.FieldType;
 import org.zenframework.z8.server.db.sql.FormatOptions;
 import org.zenframework.z8.server.db.sql.SqlToken;
 import org.zenframework.z8.server.db.sql.functions.string.Concat;
-import org.zenframework.z8.server.exceptions.db.UnknownDatabaseException;
-import org.zenframework.z8.server.types.datespan;
 
 public class Add extends Operator {
 	public Add(SqlToken left, Operation operation, SqlToken right) {
@@ -16,32 +14,10 @@ public class Add extends Operator {
 	@Override
 	public String format(DatabaseVendor vendor, FormatOptions options, boolean logicalContext) {
 		switch(operation) {
-		case Add: {
-			if(left.type() == FieldType.String) {
-				return new Concat(left, right).format(vendor, options);
-			}
-			return left.format(vendor, options) + "+" + right.format(vendor, options);
-		}
-
-		case Sub: {
-			String left = this.left.format(vendor, options);
-			String right = this.right.format(vendor, options);
-
-			if(this.left.isDate() && this.right.isDate()) {
-				switch(vendor) {
-				case Oracle:
-					return "((EXTRACT(DAY FROM (" + left + "-" + right + ") DAY TO SECOND)*" + datespan.TicksPerDay + ")+(EXTRACT(HOUR FROM (" + left + "-" + right + ") DAY TO SECOND)*" + datespan.TicksPerHour + ")+(EXTRACT(MINUTE FROM (" + left + "-" + right
-							+ ") DAY TO SECOND)*" + datespan.TicksPerMinute + ")+(EXTRACT(SECOND FROM (" + left + "-" + right + ") DAY TO SECOND)*" + datespan.TicksPerSecond + "))";
-				case SqlServer:
-					return "((convert(numeric(19,0), DATEDIFF(mi, " + right + ", " + left + "))*" + datespan.TicksPerMinute + ")+DatePart(ss, " + left + "-" + right + ")*" + datespan.TicksPerSecond + "+DatePart(ms, " + left + "-" + right + "))";
-				default:
-					throw new UnknownDatabaseException();
-				}
-			}
-
-			return left + "-" + right;
-		}
-
+		case Add:
+			return left.type() == FieldType.String ? new Concat(left, right).format(vendor, options) : left + "+" + right;
+		case Sub:
+			return left.format(vendor, options) + "-" + right.format(vendor, options);
 		default:
 			throw new UnsupportedOperationException();
 		}
