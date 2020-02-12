68d0650b96f1e7b72a9e9d7d3278f840d9aa94fc
oleg-l <oleg.lekshin@gmail.com>
2020-02-12 14:40:22 +0300

    pde ok

--- org.zenframework.z8.pde/src/main/java/org/zenframework/z8/pde/BuildPathManager.java
+++ org.zenframework.z8.pde/src/main/java/org/zenframework/z8/pde/BuildPathManager.java
@@ -1,76 +1,70 @@
 package org.zenframework.z8.pde;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.resources.ICommand;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
-
 import org.zenframework.z8.pde.build.Z8ProjectBuilder;
 
 public class BuildPathManager {
-	final static String JAVA_OUTPUT_DEFAULT_FOLDER = "src";
-	final static String JAVA_CLASS_DEFAULT_FOLDER = "classes";
-	final static String DOCS_DEFAULT_FOLDER = "docs";
+	public final static String JAVA_OUTPUT_DEFAULT_FOLDER = "./.java";
+	//public final static String JAVA_CLASS_DEFAULT_FOLDER = "classes";
+	//public final static String DOCS_DEFAULT_FOLDER = "docs";
 
-	final static String JAVA_OUTPUT_PATH_KEY = "JavaSource";
-	final static String CLASS_OUTPUT_PATH_KEY = "JavaClasses";
-	final static String DOCS_OUTPUT_PATH_KEY = "Docs";
+	public final static String JAVA_OUTPUT_PATH_KEY = "JavaSource";
+	//public final static String CLASS_OUTPUT_PATH_KEY = "JavaClasses";
+	//public final static String DOCS_OUTPUT_PATH_KEY = "Docs";
 
 	static public IPath getJavaOutputPath(IProject project) {
 		return getPath(project, JAVA_OUTPUT_PATH_KEY, JAVA_OUTPUT_DEFAULT_FOLDER);
 	}
 
-	static public IPath getClassOutputPath(IProject project) {
+	/*static public IPath getClassOutputPath(IProject project) {
 		return getPath(project, CLASS_OUTPUT_PATH_KEY, JAVA_CLASS_DEFAULT_FOLDER);
-	}
+	}*/
 
-	static public IPath getDocsOutputPath(IProject project) {
+	/*static public IPath getDocsOutputPath(IProject project) {
 		return getPath(project, DOCS_OUTPUT_PATH_KEY, DOCS_DEFAULT_FOLDER);
-	}
+	}*/
 
 	/*
 	 * static public IPath getWebInfPath(IProject project) { return
 	 * getPath(project, WEBINF_PATH_KEY, null); }
 	 */
 
-	static protected IPath getPath(IProject project, String key, String defaultFolder) {
+	static protected IPath getPath(IProject project, String key, String defaultValue) {
 		IPath projectPath = project.getLocation();
 
 		try {
-			IPath outputPath = null;
+			IProjectDescription desc = project.getDescription();
+			List<ICommand> commands = Arrays.asList(desc.getBuildSpec());
+			String value = null;
 
-			ICommand[] commands = project.getDescription().getBuildSpec();
-
-			for(ICommand command : commands) {
-				if(Z8ProjectBuilder.Id.equals(command.getBuilderName())) {
+			for (ICommand command : commands) {
+				if (Z8ProjectBuilder.Id.equals(command.getBuilderName())) {
 					Map<String, String> arguments = command.getArguments();
-
-					if(arguments != null) {
-						String value = arguments.get(key);
-
-						if(value != null) {
-							outputPath = new Path(value);
-						}
-					}
-
+					value = arguments != null ? arguments.get(key) : null;
 					break;
 				}
 			}
 
-			if(outputPath != null && !outputPath.isAbsolute()) {
-				if(outputPath.isAbsolute()) {
-					return outputPath;
-				}
+			if (value == null)
+				value = defaultValue;
 
-				return projectPath.append(outputPath);
+			if (value != null) {
+				IPath outputPath = new Path(value);
+				return outputPath.isAbsolute() ? outputPath : projectPath.append(outputPath);
 			}
 		} catch(CoreException e) {
 			Plugin.log(e);
 		}
 
-		return defaultFolder != null ? projectPath.append(defaultFolder) : projectPath;
+		return defaultValue != null ? projectPath.append(defaultValue) : projectPath;
 	}
 }
--- org.zenframework.z8.pde/src/main/java/org/zenframework/z8/pde/Z8ProjectNature.java
+++ org.zenframework.z8.pde/src/main/java/org/zenframework/z8/pde/Z8ProjectNature.java
@@ -7,8 +7,8 @@ import org.eclipse.core.resources.ICommand;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IProjectNature;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
-
 import org.zenframework.z8.pde.build.Z8ProjectBuilder;
 
 public class Z8ProjectNature implements IProjectNature {
@@ -19,17 +19,25 @@ public class Z8ProjectNature implements IProjectNature {
 
 	@Override
 	public void configure() throws CoreException {
-		if(m_project == null) {
+		if(m_project == null)
 			return;
-		}
-		IProjectDescription d = m_project.getDescription();
-		ICommand command = d.newCommand();
-		Map<String, String> args = new HashMap<String, String>(3);
-		args.put("JavaSource", "./.java");
-		command.setArguments(args);
-		command.setBuilderName(Z8ProjectBuilder.Id);
-		d.setBuildSpec(new ICommand[] { command });
-		m_project.setDescription(d, 0, null);
+
+		IProjectDescription description = m_project.getDescription();
+		ICommand[] oldBuildSpec = description.getBuildSpec();
+
+		if (getZ8CommandIndex(oldBuildSpec) >= 0)
+			return;
+		
+		ICommand[] newBuildSpec = new ICommand[oldBuildSpec.length + 1];
+		System.arraycopy(oldBuildSpec, 0, newBuildSpec, 1, oldBuildSpec.length);
+		ICommand newCommand = description.newCommand();
+		Map<String, String> args = new HashMap<String, String>();
+		args.put(BuildPathManager.JAVA_OUTPUT_PATH_KEY, BuildPathManager.JAVA_OUTPUT_DEFAULT_FOLDER);
+		newCommand.setArguments(args);
+		newCommand.setBuilderName(Z8ProjectBuilder.Id);
+		newBuildSpec[0] = newCommand;
+		description.setBuildSpec(newBuildSpec);
+		m_project.setDescription(description, IResource.FORCE, null);
 	}
 
 	@Override
@@ -47,4 +55,11 @@ public class Z8ProjectNature implements IProjectNature {
 		m_project = project;
 	}
 
+	private static int getZ8CommandIndex(ICommand[] buildSpec) {
+		for (int i = 0; i < buildSpec.length; ++i) {
+			if (buildSpec[i].getBuilderName().equals(Z8ProjectBuilder.Id))
+				return i;
+		}
+		return -1;
+	}
 }
