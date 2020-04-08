05c633d32dfd55ff0ec28a13bc056e016e099b53
oleg-l <oleg.lekshin@gmail.com>
2020-04-08 18:53:50 +0300

    pde & designer moved to https://github.com/zenframework/z8-eclipse-plugins

--- org.zenframework.z8.pde/src/main/java/org/zenframework/z8/pde/navigator/IAttributedProvider.java
+++
@@ -1,8 +0,0 @@
-package org.zenframework.z8.pde.navigator;
-
-import org.zenframework.z8.compiler.core.IAttributed;
-
-public interface IAttributedProvider {
-
-	public IAttributed getAttributed();
-}
