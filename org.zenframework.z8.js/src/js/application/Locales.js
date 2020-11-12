ebd7f02318383150de01cff5f3d8f72b833fbfb0
Koronchik Vadim <koronchik@gmail.com>
2020-11-12 16:12:46 +0300

    Update localization support

--- org.zenframework.z8.js/src/js/application/Locales.js
+++ org.zenframework.z8.js/src/js/application/Locales.js
@@ -1,16 +1,13 @@
-var ru = {
-	'Login.login.text': 'Логин',
-	'Login.login.placeholder': 'Логин',
-	'Login.password.text': 'Пароль',
-	'Login.password.placeholder': 'Пароль'
-};
+Z8.loadMessages('ru', {
+    'Login.login.text': 'Логин',
+    'Login.login.placeholder': 'Логин',
+    'Login.password.text': 'Пароль',
+    'Login.password.placeholder': 'Пароль'
+});
 
-var en = {
-	'Login.login.text': 'Login',
-	'Login.login.placeholder': 'Login',
-	'Login.password.text': 'Password',
-	'Login.password.placeholder': 'Password'
-};
-
-Z8.locales.ru = Z8.apply(Z8.locales.ru, ru);
-Z8.locales.en = Z8.apply(Z8.locales.en, en);
+Z8.loadMessages('en', {
+    'Login.login.text': 'Login',
+    'Login.login.placeholder': 'Login',
+    'Login.password.text': 'Password',
+    'Login.password.placeholder': 'Password'
+});
