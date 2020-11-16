380bf86b3724393d893301211da0256b1dc9d86e
oleg-l <oleg.lekshin@gmail.com>
2020-11-16 13:13:12 +0300

    Template load refactoring

--- org.zenframework.z8.servlet/src/main/java/org/zenframework/z8/web/server/APIDocAdapter.java
+++ org.zenframework.z8.servlet/src/main/java/org/zenframework/z8/web/server/APIDocAdapter.java
@@ -1,32 +1,35 @@
 package org.zenframework.z8.web.server;
 
-import freemarker.template.Configuration;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
-import freemarker.template.TemplateExceptionHandler;
-import org.zenframework.z8.server.apidocs.DocBuilder;
-import org.zenframework.z8.server.apidocs.dto.Documentation;
-import org.zenframework.z8.server.request.ContentType;
-import org.zenframework.z8.server.request.Loader;
-import org.zenframework.z8.server.runtime.OBJECT;
-import org.zenframework.z8.server.utils.IOUtils;
-import org.zenframework.z8.web.servlet.Servlet;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
-import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.zenframework.z8.server.apidocs.DocBuilder;
+import org.zenframework.z8.server.apidocs.dto.Documentation;
+import org.zenframework.z8.server.engine.Runtime;
+import org.zenframework.z8.server.json.Json;
+import org.zenframework.z8.server.request.ContentType;
+import org.zenframework.z8.server.runtime.OBJECT;
+import org.zenframework.z8.web.servlet.Servlet;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
 
 public class APIDocAdapter extends Adapter {
 
-	private static final String APIClassesPath = "META-INF/api_classes.list";
-	static private final String templateName = "api_documentation.html";
-	static private final String AdapterPath = "/apidoc";
+	private static final String templateName = "api_documentation.html";
+	private static final String AdapterPath = "/apidoc";
+
+	private Documentation documentation;
+	private Template temp;
 
 	public APIDocAdapter(Servlet servlet) {
 		super(servlet);
@@ -36,24 +39,10 @@ public class APIDocAdapter extends Adapter {
 	public boolean canHandleRequest(HttpServletRequest request) {
 		return request.getServletPath().endsWith(AdapterPath);
 	}
-	
+
 	@Override
 	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		Collection<OBJECT.CLASS<? extends OBJECT>> entities = new ArrayList<>();
-		URL resource = getClass().getClassLoader().getResource(APIClassesPath);
-		if (resource == null) {
-			return;
-		}
-		String rawText = IOUtils.readText(resource);
-		if (rawText.isEmpty()) {
-			return;
-		}
-		Arrays.stream(rawText.split("\\n"))
-				.forEach(className -> entities.add((OBJECT.CLASS<? extends OBJECT>) Loader.loadClass(className)));
-		Documentation documentation = new DocBuilder().build(entities);
-
-		Template temp = APIDocAdapter.getFreeMakerCfg().getTemplate(templateName);
-
+		initTemplate();
 		StringWriter stringWriter = new StringWriter();
 		try {
 			temp.process(documentation, stringWriter);
@@ -63,15 +52,33 @@ public class APIDocAdapter extends Adapter {
 		InputStream inputStream = new ByteArrayInputStream(stringWriter.toString().getBytes());
 		writeResponse(response, inputStream, ContentType.Html);
 	}
-	
+
+	private void initTemplate() {
+		if (temp != null)
+			return;
+
+		Collection<OBJECT.CLASS<? extends OBJECT>> apiClasses = new ArrayList<>();
+		for (OBJECT.CLASS<? extends OBJECT> request : Runtime.instance().requests())
+			if (request.hasAttribute(Json.apiDescription.get()))
+				apiClasses.add(request);
+		documentation = new DocBuilder().build(apiClasses);
+		try {
+			temp = APIDocAdapter.getFreeMakerCfg().getTemplate(templateName);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 	private static Configuration getFreeMakerCfg() {
-		Configuration freeMarkerCfg = new Configuration(Configuration.VERSION_2_3_30);
+		// TODO Upgrade freemarker to 2.3.30
+		Configuration freeMarkerCfg = new Configuration();
 		freeMarkerCfg.setClassForTemplateLoading(APIDocAdapter.class, "/templates/");
 		freeMarkerCfg.setDefaultEncoding("UTF-8");
 		freeMarkerCfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-		freeMarkerCfg.setLogTemplateExceptions(false);
-		freeMarkerCfg.setWrapUncheckedExceptions(true);
-		freeMarkerCfg.setFallbackOnNullLoopVariable(false);
+		//freeMarkerCfg.setLogTemplateExceptions(false);
+		//freeMarkerCfg.setWrapUncheckedExceptions(true);
+		//freeMarkerCfg.setFallbackOnNullLoopVariable(false);
 		return freeMarkerCfg;
 	}
+
 }
