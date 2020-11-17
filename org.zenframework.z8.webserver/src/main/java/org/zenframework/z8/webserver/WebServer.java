dcfea78ce85fc484759b041ae3fc893a5d844c21
Силантьев Алексей <leshka.oo@yandex.ru>
2020-11-17 09:22:05 +0400

    Added web server configuration with Kerberos authentication support

--- org.zenframework.z8.webserver/src/main/java/org/zenframework/z8/webserver/WebServer.java
+++ org.zenframework.z8.webserver/src/main/java/org/zenframework/z8/webserver/WebServer.java
@@ -1,15 +1,6 @@
 package org.zenframework.z8.webserver;
 
-import java.io.IOException;
 import java.rmi.RemoteException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.eclipse.jetty.security.ConstraintMapping;
-import org.eclipse.jetty.security.ConstraintSecurityHandler;
-import org.eclipse.jetty.security.SpnegoLoginService;
-import org.eclipse.jetty.server.Authentication;
-import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.SessionIdManager;
 import org.eclipse.jetty.server.handler.ContextHandler;
@@ -17,14 +8,11 @@ import org.eclipse.jetty.server.session.HashSessionIdManager;
 import org.eclipse.jetty.server.session.HashSessionManager;
 import org.eclipse.jetty.server.session.SessionHandler;
 import org.eclipse.jetty.servlets.gzip.GzipHandler;
-import org.eclipse.jetty.util.security.Constraint;
 import org.zenframework.z8.server.config.ServerConfig;
 import org.zenframework.z8.server.engine.IWebServer;
 import org.zenframework.z8.server.engine.RmiServer;
 import org.zenframework.z8.server.logs.Trace;
 import org.zenframework.z8.server.types.guid;
-import org.zenframework.z8.web.server.SingleSignOnAdapter;
-import org.zenframework.z8.webserver.spnego.CustomSpnegoAuthenticator;
 
 /**
  * {@link WebServer#launch(org.zenframework.z8.server.config.ServerConfig)} method is the entrypoint,
@@ -55,39 +43,8 @@ public class WebServer extends RmiServer implements IWebServer {
 		SessionIdManager idmanager = new HashSessionIdManager();
 		server.setSessionIdManager(idmanager);
 
-		final String domainRealm = ServerConfig.domainRealm();
-		final String spnegoPropertiesPath = ServerConfig.spnegoPropertiesPath();
-		Constraint constraint = new Constraint();
-		constraint.setName(Constraint.__SPNEGO_AUTH);
-		constraint.setRoles(new String[]{domainRealm});
-		constraint.setAuthenticate(true);
-
-		ConstraintMapping cm = new ConstraintMapping();
-		cm.setConstraint(constraint);
-		cm.setPathSpec(SingleSignOnAdapter.AdapterPath);
-
-		SpnegoLoginService loginService = new SpnegoLoginService(domainRealm, spnegoPropertiesPath);
-
-		ConstraintSecurityHandler securityHandler = new ConstraintSecurityHandler();
-		securityHandler.setAuthenticator(new CustomSpnegoAuthenticator());
-		securityHandler.setLoginService(loginService);
-		securityHandler.setConstraintMappings(new ConstraintMapping[]{cm});
-		securityHandler.setRealmName(domainRealm);
-
-		SessionHandler sessions = new SessionHandler(new HashSessionManager()) {
-			/**
-			 * The method additionally extracts from the session {@link Authentication} object
-			 * to put it in the request
-			 */
-			@Override
-			public void doHandle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-				Authentication auth = (Authentication) baseRequest.getSession().getAttribute("authentication");
-				if (auth != null) {
-					baseRequest.setAuthentication(auth);
-				}
-				super.doHandle(target, baseRequest, request, response);
-			}
-		};
+		// Create the SessionHandler (wrapper) to handle the sessions
+		SessionHandler sessions = new SessionHandler(new HashSessionManager());
 		context.setHandler(sessions);
 
 		GzipHandler gzipHandler = new GzipHandler();
@@ -96,8 +53,8 @@ public class WebServer extends RmiServer implements IWebServer {
 		gzipHandler.addIncludedMethods(ServerConfig.webServerGzipMethods());
 		gzipHandler.addIncludedPaths(ServerConfig.webServerGzipPaths());
 
-		securityHandler.setHandler(gzipHandler);
-		sessions.setHandler(securityHandler);
+		// Put handler inside of SessionHandler
+		sessions.setHandler(gzipHandler);
 	}
 
 	@Override
