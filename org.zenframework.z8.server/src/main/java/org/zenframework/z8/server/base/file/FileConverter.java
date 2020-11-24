2720c5999565e13607d8a7a7bbc2eeb027105b99
RapidSilver <40090762+RapidSilver@users.noreply.github.com>
2020-11-24 12:18:27 +0400

    Update FileConverter.java

--- org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/base/file/FileConverter.java
+++ org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/base/file/FileConverter.java
@@ -1,31 +1,33 @@
 package org.zenframework.z8.server.base.file;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.Map;
 
 import org.apache.commons.io.FilenameUtils;
-import org.artofsolving.jodconverter.OfficeDocumentConverter;
-import org.artofsolving.jodconverter.office.DefaultOfficeManagerConfiguration;
-import org.artofsolving.jodconverter.office.ExternalOfficeManagerConfiguration;
-import org.artofsolving.jodconverter.office.OfficeException;
-import org.artofsolving.jodconverter.office.OfficeManager;
+//  импорт не проходит так как нет либы в z8
+//import org.jodconverter.core.office.OfficeException;
+//import org.jodconverter.core.office.OfficeManager;
+//import org.jodconverter.core.office.OfficeUtils;
+//import org.jodconverter.local.JodConverter;
+//import org.jodconverter.local.office.ExternalOfficeManager;
+//import org.jodconverter.local.office.LocalOfficeManager;
+
 import org.zenframework.z8.server.config.ServerConfig;
 import org.zenframework.z8.server.logs.Trace;
 import org.zenframework.z8.server.runtime.RLinkedHashMap;
+import org.zenframework.z8.server.types.binary;
 import org.zenframework.z8.server.types.bool;
 import org.zenframework.z8.server.types.file;
 import org.zenframework.z8.server.types.string;
-import org.zenframework.z8.server.utils.ArrayUtils;
-import org.zenframework.z8.server.utils.EmlUtils;
-import org.zenframework.z8.server.utils.PdfUtils;
-import org.zenframework.z8.server.utils.PrimaryUtils;
+import org.zenframework.z8.server.utils.*;
 
 public class FileConverter {
-
 	private static final int OFFICE_PORT = 8100;
-	
+
 	public static final String PDF_EXTENSION = "pdf";
 
 	public static final string Background = new string("background");
@@ -33,14 +35,19 @@ public class FileConverter {
 
 	private static OfficeManager officeManager;
 
-	private FileConverter() {}
+	private FileConverter() {
+	}
+
+	public static file z8_convertToPdf(binary source, string type, file target) {
+		return new file(convertToPdf(source.get(), type.get(), target.toFile(), Collections.<String, String> emptyMap()));
+	}
 
 	public static file z8_convertToPdf(file source, file target) {
 		return new file(convertToPdf(source.toFile(), target.toFile()));
 	}
 
 	public static File convertToPdf(File source, File target) {
-		return convertToPdf(source, target, Collections.<String, String>emptyMap());
+		return convertToPdf(source, target, Collections.<String, String> emptyMap());
 	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
@@ -48,43 +55,62 @@ public class FileConverter {
 		return new file(convertToPdf(source.toFile(), target.toFile(), PrimaryUtils.unwrapStringMap(parameters)));
 	}
 
+	public static file z8_convertToDocx(file source, file target) {
+		return z8_convertToDocx(source.binary(), target);
+	}
+
+	public static file z8_convertToDocx(binary source, file target) {
+		return new file(convertToDocx(source.get(), target.toFile()));
+	}
+
 	public static File convertToPdf(File source, File target, Map<String, String> parameters) {
-		String extension = getExtension(source);
+		try {
+			String type = FilenameUtils.getExtension(source.getName()).toLowerCase();
+			return convertToPdf(new FileInputStream(source), type, target, parameters);
+		} catch(Throwable e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static File convertToPdf(InputStream in, String type, File target, Map<String, String> parameters) {
 		boolean reset = Boolean.parseBoolean(parameters.get(Reset.get()));
 
-		if (reset && target.exists())
+		if(reset && target.exists())
 			target.delete();
-		if (!target.exists()) {
+
+		if(!target.exists()) {
 			target.getParentFile().mkdirs();
 			try {
-				if (isTextExtension(extension))
-					PdfUtils.textToPdf(source, target);
-				else if (isImageExtension(extension))
-					PdfUtils.imageToPdf(source, target);
-				else if (isEmailExtension(extension))
-					PdfUtils.textToPdf(EmlUtils.emailToString(source), target);
-				else if (isOfficeExtension(extension))
-					convertOfficeToPdf(source, target);
-			} catch (IOException e) {
-				Trace.logError("Can't convert " + source + " to " + target, e);
+				if(isTextExtension(type))
+					PdfUtils.textToPdf(in, target);
+				else if(isImageExtension(type))
+					throw new UnsupportedOperationException();
+				else if(isEmailExtension(type))
+					PdfUtils.textToPdf(EmlUtils.emailToString(in), target);
+				else if(isOfficeExtension(type))
+					convertAction(in, target);
+			} catch(IOException e) {
+				throw new RuntimeException(e);
+			} finally {
+				IOUtils.closeQuietly(in);
 			}
 		}
 
 		String background = parameters.get(Background.get());
 		File backgroundFile;
-		if (background != null && (backgroundFile = new File(Folders.Base, background)).exists()) {
-			source = target;
+		if(background != null && (backgroundFile = new File(Folders.Base, background)).exists()) {
+			File source = target;
 			target = new File(source.getParentFile(), addSuffix(source.getName(), "-background"));
-			if (reset && target.exists())
+			if(reset && target.exists())
 				target.delete();
-			if (!target.exists()) {
-				extension = FilenameUtils.getExtension(background).toLowerCase();
+			if(!target.exists()) {
+				type = FilenameUtils.getExtension(background).toLowerCase();
 				try {
-					if (isPdfExtension(extension))
+					if(isPdfExtension(type))
 						PdfUtils.insertBackgroundPdf(source, target, backgroundFile, false);
 					else
 						PdfUtils.insertBackgroundImg(source, target, backgroundFile);
-				} catch (IOException e) {
+				} catch(IOException e) {
 					throw new RuntimeException(e);
 				}
 			}
@@ -93,20 +119,8 @@ public class FileConverter {
 		return target;
 	}
 
-	public static string z8_getExtension(file file) {
-		return new string(file.extension());
-	}
-
-	public static String getExtension(File file) {
-		return FilenameUtils.getExtension(file.getName()).toLowerCase();
-	}
-
-	public static string z8_getExtension(string fileName) {
-		return new string(getExtension(fileName.get()));
-	}
-
-	public static String getExtension(String fileName) {
-		return FilenameUtils.getExtension(fileName).toLowerCase();
+	public static File convertToDocx(InputStream input, File target) {
+		return convertAction(input, target);
 	}
 
 	public static bool z8_isConvertableToPdf(string extension) {
@@ -114,9 +128,7 @@ public class FileConverter {
 	}
 
 	public static boolean isConvertableToPdf(String extension) {
-		return isPdfExtension(extension) || isTextExtension(extension)
-				|| isImageExtension(extension) || isOfficeExtension(extension)
-				|| isEmailExtension(extension);
+		return isPdfExtension(extension) || isTextExtension(extension) || isImageExtension(extension) || isOfficeExtension(extension) || isEmailExtension(extension);
 	}
 
 	public static bool z8_isPdfExtension(string extension) {
@@ -160,61 +172,48 @@ public class FileConverter {
 	}
 
 	public static void startOfficeManager() {
-		if (officeManager != null)
+		if(officeManager != null)
 			return;
 
 		try {
-			// Try to connect to an existing instance of openoffice
-			ExternalOfficeManagerConfiguration externalProcessOfficeManager = new ExternalOfficeManagerConfiguration();
-			externalProcessOfficeManager.setConnectOnStart(true);
-			externalProcessOfficeManager.setPortNumber(OFFICE_PORT);
-			officeManager = externalProcessOfficeManager.buildOfficeManager();
+			officeManager = ExternalOfficeManager.builder().install().connectOnStart(true).portNumber(OFFICE_PORT).build();
 			officeManager.start();
-			Trace.logEvent("Connected to an existing OpenOffice process, port " + OFFICE_PORT);
-		} catch (OfficeException e) {
-			String officeHome = ServerConfig.officeHome();
-
+		} catch(OfficeException e) {
 			try {
 				Trace.logEvent("Can't connect to an existing OpenOffice process: " + e.getMessage());
-				//Start a new openoffice instance
-				officeManager = new DefaultOfficeManagerConfiguration()
-						.setOfficeHome(officeHome).setPortNumber(OFFICE_PORT)
-						.buildOfficeManager();
+				officeManager = LocalOfficeManager.builder().install().officeHome(ServerConfig.officeHome()).portNumbers(OFFICE_PORT).build();
 				officeManager.start();
-				Trace.logEvent("New OpenOffice '" + officeHome + "' process created, port "
-						+ OFFICE_PORT);
-			} catch (Throwable e1) {
-				Trace.logError("Could not start OpenOffice '" + officeHome + "'", e1);
-				officeManager = null;
+				Trace.logEvent("New OpenOffice '" + ServerConfig.officeHome() + "' process created, port " + OFFICE_PORT);
+			} catch(OfficeException e1) {
+				Trace.logError("Could not start OpenOffice '" + ServerConfig.officeHome() + "'", e1);
 			}
 		}
+
 	}
 
 	public static void stopOfficeManager() {
-		if (officeManager == null)
+		if(officeManager == null)
 			return;
-		officeManager.stop();
+		OfficeUtils.stopQuietly(officeManager);
 		officeManager = null;
 	}
 
-	private static void convertOfficeToPdf(File sourceFile, File convertedFile) throws IOException {
+	private static File convertAction(InputStream input, File target) {
 		try {
-			getOfficeDocumentConverter().convert(sourceFile, convertedFile);
-		} catch (NullPointerException e) {
-			throw new IOException("OpenOffice process is not started", e);
+			startOfficeManager();
+			JodConverter.convert(input).to(target).execute();
+			return target;
+		} catch(Throwable e) {
+			throw new RuntimeException(e);
 		}
 	}
 
-	private static OfficeDocumentConverter getOfficeDocumentConverter() {
-		startOfficeManager();
-		return new OfficeDocumentConverter(officeManager);
-	}
 
 	private static String addSuffix(String filename, String suffix) {
-		if (filename == null)
+		if(filename == null)
 			return null;
 		int extIndex = FilenameUtils.indexOfExtension(filename);
-		if (extIndex < 0)
+		if(extIndex < 0)
 			return filename + suffix;
 		return filename.substring(0, extIndex) + suffix + filename.substring(extIndex);
 	}
