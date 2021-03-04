a2f3d0960f4644f86dd682d97083a6080298146e
Oleg S. Lekshin <oleg.lekshin@gmail.com>
2021-03-04 17:00:49 +0300

    Linux shell scripts bug fix

--- org.zenframework.z8.resources/src/bin/start.sh
+++ org.zenframework.z8.resources/src/bin/start.sh
@@ -1,27 +1,8 @@
 #!/usr/bin/env sh
 
-#export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-
-HOST=\$(hostname -I | awk '{print \$1}')
-FORM_CONTENT_SIZE=15000000
-
 HOME="\$( cd "\$(dirname "\$0")/.." ; pwd -P )"
 LOG="\$HOME/log"
-WEB="\$HOME/web"
-WORK="\$HOME/work"
 
-BOOT_CP="\$HOME/lib/org.zenframework.z8.boot-${project.z8Version}.jar"
+mkdir \$LOG
 
-JAVA_OPTS="-Xmx2048M"
-JAVA_OPTS="\$JAVA_OPTS -Xbootclasspath/p:\$BOOT_CP"
-JAVA_OPTS="\$JAVA_OPTS -Dorg.eclipse.jetty.server.Request.maxFormContentSize=\$FORM_CONTENT_SIZE"
-JAVA_OPTS="\$JAVA_OPTS -Dorg.mortbay.http.HttpRequest.maxFormContentSize=\$FORM_CONTENT_SIZE"
-JAVA_OPTS="\$JAVA_OPTS -Djava.rmi.server.hostname=\$HOST"
-JAVA_OPTS="\$JAVA_OPTS -Dz8.web.server.webapp=\$WEB"
-
-# Debug mode
-#JAVA_OPTS="\$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=9999,server=y,suspend=n"
-
-export JAVA_OPTS
-
-(cd "\$WORK" && "\$HOME/bin/${project.name}" -server webserver) &
+(cd "\$WORK" && "\$HOME/bin/service.sh" 1>\$LOG/out.log 2>\$LOG/err.log) &
