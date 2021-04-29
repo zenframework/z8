3e31682343b08a0e50c361fa3938b1999a1db922
Силантьев Алексей <leshka.oo@yandex.ru>
2021-04-29 12:04:34 +0300

    feat: entity actions support, add anchors to the key fields

---
+++ org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/apidocs/dto/EntityAction.java
@@ -0,0 +1,31 @@
+package org.zenframework.z8.server.apidocs.dto;
+
+import org.zenframework.z8.server.json.parser.JsonArray;
+import org.zenframework.z8.server.json.parser.JsonObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EntityAction extends BaseInfo {
+	private List<FieldDescription> parameters;
+	
+	public EntityAction(String name, String description) {
+		super(name, description);
+		parameters = new ArrayList<>();
+	}
+
+	public List<FieldDescription> getParameters() {
+		return parameters;
+	}
+
+	public String getJson() {
+		JsonArray parameters = new JsonArray();
+		for (FieldDescription parameter : getParameters()) {
+			JsonObject obj = new JsonObject();
+			obj.put("id", parameter.name);
+			obj.put("value", "");
+			parameters.add(obj);
+		}
+		return parameters.toString();
+	}
+}
