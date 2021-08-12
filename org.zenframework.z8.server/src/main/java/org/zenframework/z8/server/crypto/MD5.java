98fd1cb27272739e81b6318f0416122dad18eba6
an.volkov <an.volkov@tinkoff.ru>
2021-08-13 00:31:51 +0300

    SHA-256 hex function.

--- org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/crypto/MD5.java
+++ org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/crypto/MD5.java
@@ -1,6 +1,7 @@
 package org.zenframework.z8.server.crypto;
 
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -8,9 +9,9 @@ import org.zenframework.z8.server.types.string;
 import org.zenframework.z8.server.utils.StringUtils;
 
 public class MD5 {
-	public static String hex(String value) {
-		MessageDigest messageDigest = null;
 
+	public static String hex(String value) {
+		MessageDigest messageDigest;
 		try {
 			messageDigest = MessageDigest.getInstance("MD5");
 			messageDigest.reset();
@@ -22,7 +23,22 @@ public class MD5 {
 		}
 	}
 
+	public static String sha256hex(String value) {
+		MessageDigest messageDigest;
+		try {
+			messageDigest = MessageDigest.getInstance("SHA-256");
+			byte[] digest = messageDigest.digest(value.getBytes(StandardCharsets.UTF_8));
+			return StringUtils.padLeft(new BigInteger(1, digest).toString(16), 32, '0');
+		} catch(NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 	public static string z8_hex(string value) {
 		return new string(hex(value.get()));
+	}
+
+	public static string z8_sha256hex(string value) {
+		return new string(sha256hex(value.get()));
 	}
 }
