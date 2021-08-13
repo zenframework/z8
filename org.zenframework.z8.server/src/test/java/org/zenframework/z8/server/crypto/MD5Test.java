f07b9a4c19c030fa13e91291990ada6980699c14
an.volkov <an.volkov@tinkoff.ru>
2021-08-13 20:56:33 +0300

    refactoring.

--- org.zenframework.z8.server/src/test/java/org/zenframework/z8/server/crypto/MD5Test.java
+++ org.zenframework.z8.server/src/test/java/org/zenframework/z8/server/crypto/MD5Test.java
@@ -9,14 +9,12 @@ public class MD5Test {
     @Test
     public void testHex() {
         String hex = MD5.hex("test-hex-value");
-        System.out.println(hex);
         assertEquals("168f6540493857c991e5748b3c24113a", hex);
     }
 
     @Test
     public void testSha256Hex() {
         String hex = MD5.sha256hex("test-hex-value");
-        System.out.println(hex);
         assertEquals("a3476cac4c8f229c17a32665b04aefb71bcf40db030c9c0552351f1f131f86dc", hex);
     }
 }
