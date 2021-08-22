ea7a79a07ae48605e60a81dfdf66aa9087a3d552
an.volkov <an.volkov@tinkoff.ru>
2021-08-22 15:39:32 +0300

    PRO-2402 bytes as param.

--- org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/crypto/Digest.java
+++ org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/crypto/Digest.java
@@ -23,11 +23,11 @@ public class Digest {
 		return digest(value, "MD5");
 	}
 
-	public static string z8_sha256(string value) {
-		return new string(sha256(value.get()));
+	public static string z8_sha256(byte[] bytes) {
+		return new string(sha256(bytes));
 	}
 
-	public static String sha256(String value) {
+	public static String sha256(byte[] value) {
 		return digest(value, "SHA-256");
 	}
 
@@ -40,11 +40,15 @@ public class Digest {
 	}
 
 	private static String digest(String value, String algorithm) {
+		return digest(value.getBytes(), algorithm);
+	}
+
+	private static String digest(byte[] bytes, String algorithm) {
 		MessageDigest messageDigest;
 		try {
 			messageDigest = MessageDigest.getInstance(algorithm);
 			messageDigest.reset();
-			messageDigest.update(value.getBytes());
+			messageDigest.update(bytes);
 			byte[] digest = messageDigest.digest();
 			return StringUtils.padLeft(new BigInteger(1, digest).toString(16), 32, '0');
 		} catch(NoSuchAlgorithmException e) {
