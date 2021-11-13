2cfdbde929c194bc169a0d2be357e6a80e8c2fb3
Maks <x120573@gmail.com>
2021-11-13 22:34:14 +0300

    ok

--- org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/json/parser/JsonUtils.java
+++ org.zenframework.z8.server/src/main/java/org/zenframework/z8/server/json/parser/JsonUtils.java
@@ -63,7 +63,7 @@ public class JsonUtils {
 	}
 
 	public static Object wrap(Object o) {
-		if (o == null || JsonObject.NULL.equals(o))
+		if(o == null)
 			return null;
 		else if(o instanceof primary)
 			return (primary)o;
