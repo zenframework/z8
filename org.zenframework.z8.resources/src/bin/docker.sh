136322cb33e98be6e6d6c61539b135a3ab514936
Oleg S. Lekshin <oleg.lekshin@gmail.com>
2022-08-04 19:10:30 +0300

    Update scripts

--- org.zenframework.z8.resources/src/bin/docker.sh
+++ org.zenframework.z8.resources/src/bin/docker.sh
@@ -1,26 +1,6 @@
 #!/usr/bin/env sh
 
-#export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-
-#HOST=\$(hostname -I | awk '{print \$1}')
-FORM_CONTENT_SIZE=15000000
-
 HOME="\$( cd "\$(dirname "\$0")/.." ; pwd -P )"
-LOG="\$HOME/log"
-WEB="\$HOME/web"
-WORK="\$HOME/work"
-
-BOOT_CP="\$HOME/lib/org.zenframework.z8.boot-${project.z8Version}.jar"
-
-JAVA_OPTS="\$JAVA_OPTS -Xmx2048M"
-JAVA_OPTS="\$JAVA_OPTS -Xbootclasspath/p:\$BOOT_CP"
-JAVA_OPTS="\$JAVA_OPTS -Dorg.eclipse.jetty.server.Request.maxFormContentSize=\$FORM_CONTENT_SIZE"
-JAVA_OPTS="\$JAVA_OPTS -Dorg.mortbay.http.HttpRequest.maxFormContentSize=\$FORM_CONTENT_SIZE"
-JAVA_OPTS="\$JAVA_OPTS -Dz8.web.server.webapp=\$WEB"
-#JAVA_OPTS="\$JAVA_OPTS -Djava.rmi.server.hostname=\$HOST"
-
-# Debug mode
-#JAVA_OPTS="\$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=9999,server=y,suspend=n"
 
 # Env variables, format <<PROJECT_NAME>_<VAR>>
 if [[ ! -z "\$${project.name.toUpperCase().replaceAll('\\W', '_')}_DB_SCHEMA" ]]; then
@@ -58,6 +38,4 @@ fi
 
 export JAVA_OPTS
 
-rm -f "\$WORK/*.cache"
-
-(cd "\$WORK" && "\$HOME/bin/${project.name}" -server webserver)
+"\$HOME/bin/service.sh"
--- org.zenframework.z8.resources/src/bin/service.sh
+++ org.zenframework.z8.resources/src/bin/service.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env sh
 
+### !!! NOTE !!! ###
+# If Ubuntu, change sh to bash
+### !!! NOTE !!! ###
+
 #export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 
-HOST=\$(hostname -I | awk '{print \$1}')
+# if "true" enable debug mode
+#DEBUG="true"
+
 FORM_CONTENT_SIZE=15000000
 
 HOME="\$( cd "\$(dirname "\$0")/.." ; pwd -P )"
-LOG="\$HOME/log"
 WEB="\$HOME/web"
 WORK="\$HOME/work"
 
@@ -16,11 +21,11 @@ JAVA_OPTS="\$JAVA_OPTS -Xmx2048M"
 JAVA_OPTS="\$JAVA_OPTS -Xbootclasspath/p:\$BOOT_CP"
 JAVA_OPTS="\$JAVA_OPTS -Dorg.eclipse.jetty.server.Request.maxFormContentSize=\$FORM_CONTENT_SIZE"
 JAVA_OPTS="\$JAVA_OPTS -Dorg.mortbay.http.HttpRequest.maxFormContentSize=\$FORM_CONTENT_SIZE"
-JAVA_OPTS="\$JAVA_OPTS -Djava.rmi.server.hostname=\$HOST"
 JAVA_OPTS="\$JAVA_OPTS -Dz8.web.server.webapp=\$WEB"
 
-# Debug mode
-#JAVA_OPTS="\$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=9999,server=y,suspend=n"
+if [ "\$DEBUG" == "true" ] ; then
+ JAVA_OPTS="\$JAVA_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,address=9999,server=y,suspend=n"
+fi
 
 export JAVA_OPTS
 
