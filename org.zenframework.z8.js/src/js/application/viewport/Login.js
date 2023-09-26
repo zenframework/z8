1f8fe43b865017f40328b321833ad181e57e4426
Dzhafarov <Dzhafarov@ivk.ru>
2023-09-26 13:41:03 +0300

    требования к паролю

--- org.zenframework.z8.js/src/js/application/viewport/Login.js
+++ org.zenframework.z8.js/src/js/application/viewport/Login.js
@@ -128,6 +128,25 @@ Z8.define('Z8.application.viewport.SSOLogin', {
 	},
 });
 
+Z8.define('Z8.application.viewport.ChangePassword.PasswordField', {
+	extend: 'Z8.form.field.Text',
+	shortClassName: 'ChangePassword.PasswordField',
+
+	validate: function() {
+		this.setValid(!this.checkRequirements());
+	},
+
+	checkRequirements: function() {
+		var newPassword = this.getValue() || '';
+		var appPassword = Application.password;
+		var hasUpperCase = appPassword.mustHaveUpperCase ? /[A-ZА-Я]/.test(newPassword) : true;
+		var hasLowerCase = appPassword.mustHaveLowerCase ? /[a-zа-я]/.test(newPassword) : true;
+		var hasSpecialChar = appPassword.mustHaveSpecialСhar ? /[!@#$%^&*()_+{}\[\]:;<>,.?~\\\/'"`]/.test(newPassword) : true;
+		var hasDigit = Application.password.mustHaveDigit ? /[0-9]/.test(newPassword) : true;
+		return newPassword.length < appPassword.minLength || !hasUpperCase || !hasLowerCase || !hasSpecialChar || !hasDigit;
+	}
+});
+
 Z8.define('Z8.application.viewport.ChangePassword', {
 	extend: 'Z8.window.Window',
 
@@ -142,7 +161,7 @@ Z8.define('Z8.application.viewport.ChangePassword', {
 			fn: function(control, valid) {
 				if(control == newPassword1)
 					newPassword2.validate();
-				if(this.okButton != null)
+				if(control == newPassword2 && this.okButton != null)
 					this.okButton.setEnabled(valid);
 			},
 			scope: this
@@ -159,21 +178,8 @@ Z8.define('Z8.application.viewport.ChangePassword', {
 			controls.add(password);
 		}
 
-		var appPassword = this.appPassword = Application.password;
-		var mustHaveUpperCase = appPassword.mustHaveUpperCase;
-		var mustHaveLowerCase = appPassword.mustHaveLowerCase;
-		var mustHaveSpecialСhar = appPassword.mustHaveSpecialСhar;
-		var mustHaveDigit = appPassword.mustHaveDigit;
-		var minLength = appPassword.minLength;
-		var requirements;
-		if(mustHaveUpperCase || mustHaveLowerCase || mustHaveSpecialСhar || mustHaveDigit || minLength > 0) {
-			requirements = Z8.$('ChangePassword.passwordMustContain') + (mustHaveUpperCase ? '\n' + Z8.$('ChangePassword.oneCapitalLetter') : '') + (mustHaveLowerCase ? '\n' + Z8.$('ChangePassword.oneSmallLetter') : '')
-					+ (mustHaveSpecialСhar ? '\n' + Z8.$('ChangePassword.oneSpecialChar') : '') + (mustHaveDigit ? '\n' + Z8.$('ChangePassword.oneDigit') : '') + (minLength > 0 ? '\n' + Z8.$('ChangePassword.length') + minLength : '');
-		} else {
-			requirements = Z8.$('ChangePassword.noRequirements');
-		}
-		var newPassword1 = this.newPassword1 = new Z8.form.field.Text({ label: Z8.$('ChangePassword.newPassword'), placeholder: Z8.$('ChangePassword.newPassword'), password: true, validation: validation, colSpan: 11 });
-		var passwordRequirement = this.passwordRequirement = new Button({ icon: 'fa-question', tooltip: requirements, enabled: true, cls: 'password-req', colSpan: 1 })
+		var newPassword1 = this.newPassword1 = new ChangePassword.PasswordField({ label: Z8.$('ChangePassword.newPassword'), placeholder: Z8.$('ChangePassword.newPassword'), password: true, validation: validation, colSpan: 11 });
+		var passwordRequirement = this.passwordRequirement = new Button({ icon: 'fa-question', tooltip: this.getRequirements(), enabled: true, cls: 'password-req', colSpan: 1 })
 		var newPasswordField = new Z8.form.Fieldset({ plain: true, controls: [newPassword1, passwordRequirement], colCount: 12 });
 
 		var newPassword2 = this.newPassword2 = new Z8.form.field.Text({ label: Z8.$('ChangePassword.newPassword') + ' ' + Z8.$('ChangePassword.repeat'), placeholder: Z8.$('ChangePassword.newPassword'), password: true, required: true, validation: validation, isEmptyValue: isEmptyValue });
@@ -181,6 +187,22 @@ Z8.define('Z8.application.viewport.ChangePassword', {
 		this.controls = controls.add([newPasswordField, newPassword2]);
 
 		this.callParent();
+	},
+
+	getRequirements: function() {
+		var appPassword = Application.password;
+		var mustHaveUpperCase = appPassword.mustHaveUpperCase;
+		var mustHaveLowerCase = appPassword.mustHaveLowerCase;
+		var mustHaveSpecialСhar = appPassword.mustHaveSpecialСhar;
+		var mustHaveDigit = appPassword.mustHaveDigit;
+		var minLength = appPassword.minLength;
+		if(mustHaveUpperCase || mustHaveLowerCase || mustHaveSpecialСhar || mustHaveDigit || minLength > 0) {
+			return Z8.$('ChangePassword.passwordMustContain') + (mustHaveUpperCase ? '\n - ' + Z8.$('ChangePassword.oneCapitalLetter') : '') + (mustHaveLowerCase ? '\n - ' + Z8.$('ChangePassword.oneSmallLetter') : '')
+					+ (mustHaveSpecialСhar ? '\n - ' + Z8.$('ChangePassword.oneSpecialChar') : '') + (mustHaveDigit ? '\n - ' + Z8.$('ChangePassword.oneDigit') : '')
+					+ (minLength > 0 ? '\n - ' + Z8.$('ChangePassword.length') + minLength : '');
+		} else {
+			return Z8.$('ChangePassword.noRequirements');
+		}
 	},
 
 	ok: function() {
@@ -199,17 +221,13 @@ Z8.define('Z8.application.viewport.ChangePassword', {
 			password = Application.hashPassword ? MD5.hex(password) : password;
 		}
 
-		var newPassword = this.newPassword1.getValue() || '';
-		var appPassword = this.appPassword;
-		var hasUpperCase = appPassword.mustHaveUpperCase ? /[A-ZА-Я]/.test(newPassword) : true;
-		var hasLowerCase = appPassword.mustHaveLowerCase ? /[a-zа-я]/.test(newPassword) : true;
-		var hasSpecialChar = appPassword.mustHaveSpecialСhar ? /[!@#$%^&*()_+{}\[\]:;<>,.?~\\\/'"`]/.test(newPassword) : true;
-		var hasDigit = Application.password.mustHaveDigit ? /[0-9]/.test(newPassword) : true;
-		if(newPassword.length < appPassword.minLength || !hasUpperCase || !hasLowerCase || !hasSpecialChar || !hasDigit) {
+		var newPassword1 = this.newPassword1;
+		if(newPassword1.checkRequirements()) {
 			Application.message({ text: Z8.$('ChangePassword.requirementsError'), type: 'error' });
 			this.okButton.setBusy(false);
 			return;
 		}
+		var newPassword = newPassword1.getValue() || '';
 		newPassword = Application.hashPassword ? MD5.hex(newPassword) : newPassword;
 
 		var parameters = {
