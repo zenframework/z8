3b5e9d56d692c718f95c4c803f88b41a871d7f26
Кирилл Коршунов <korshunov@ivk.ru>
2024-12-19 23:22:51 +0300

    AcceptAdapter fix

--- org.zenframework.z8.servlet/src/main/java/org/zenframework/z8/web/server/AcceptAdapter.java
+++ org.zenframework.z8.servlet/src/main/java/org/zenframework/z8/web/server/AcceptAdapter.java
@@ -1,14 +1,21 @@
 package org.zenframework.z8.web.server;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 import java.util.Map;
 
+import javax.management.RuntimeErrorException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.zenframework.z8.server.config.ServerConfig;
 import org.zenframework.z8.server.engine.IApplicationServer;
+import org.zenframework.z8.server.engine.IAuthorityCenter;
 import org.zenframework.z8.server.engine.ISession;
 import org.zenframework.z8.server.exceptions.AccessDeniedException;
+import org.zenframework.z8.server.types.file;
 import org.zenframework.z8.server.types.guid;
 
 public class AcceptAdapter extends Adapter {
@@ -34,10 +41,24 @@ public class AcceptAdapter extends Adapter {
 			throw new AccessDeniedException();
 		
 		IApplicationServer remoteServer = ServerConfig.interconnectionCenter().connect(remoteDomain);
-		if(remoteServer == null || !remoteServer.authorityCenter().checkRemoteToken(token, localDomain, login))
+		if(remoteServer == null)
+			throw new AccessDeniedException();
+		IAuthorityCenter remoteAutCenter = remoteServer.authorityCenter();
+		if(!remoteAutCenter.checkRemoteToken(token, localDomain, login))
 			throw new AccessDeniedException();
 
 		return ServerConfig.authorityCenter().trustedLogin(getLoginParameters(login, request, true), true);
+	}
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response, Map<String, String> parameters, List<file> files, ISession session) throws IOException {
+		try {
+			URI uri = new URI(request.getRequestURI());
+			response.sendRedirect(uri.getScheme() + "://" + uri.getAuthority());
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
+		
 	}
 	
 	private boolean contains(String[] strings, String value) {
