e985661050762bec036af09f58871bf50300f814
Кирилл Коршунов <korshunov@ivk.ru>
2024-12-20 16:24:30 +0300

    RedirectAdapter and AcceptAdapter
    and Crypto

---
+++ org.zenframework.z8.servlet/src/main/java/org/zenframework/z8/web/server/AcceptAdapter.java
@@ -0,0 +1,67 @@
+package org.zenframework.z8.web.server;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.zenframework.z8.server.config.ServerConfig;
+import org.zenframework.z8.server.crypto.Crypto;
+import org.zenframework.z8.server.engine.ISession;
+import org.zenframework.z8.server.exceptions.AccessDeniedException;
+import org.zenframework.z8.server.json.Json;
+import org.zenframework.z8.server.json.parser.JsonObject;
+import org.zenframework.z8.server.types.file;
+
+public class AcceptAdapter extends Adapter {
+	static private final String AdapterPath = "/accept";
+
+	@Override
+	public boolean canHandleRequest(HttpServletRequest request) {
+		return request.getServletPath().equals(AdapterPath);
+	}
+
+	@Override
+	protected ISession authorize(HttpServletRequest request, Map<String, String> parameters) throws IOException {
+		String jsonString = parameters.get("json");
+		if(jsonString == null || !UseContainerSession)
+			throw new AccessDeniedException();
+		JsonObject json = new JsonObject(Crypto.Default.decrypt(jsonString));
+		
+		if(!json.containsKey(Json.domain.get())
+		|| !json.containsKey(Json.login.get())
+		|| !json.containsKey(Json.time.get()))
+			throw new AccessDeniedException();
+		
+		long jsonTime = json.getLong(Json.time.get());
+		long currentTime = System.currentTimeMillis();
+		if(currentTime - jsonTime >= 7000)
+			throw new AccessDeniedException();
+		
+		String domain = json.getString(Json.domain.get());
+		String login = json.getString(Json.login.get());
+		String[] localDomains = ServerConfig.applicationServer().domains();
+		
+		if(domain == null || !contains(localDomains, domain))
+			throw new AccessDeniedException();
+
+		ISession session = ServerConfig.authorityCenter().trustedLogin(getLoginParameters(login, request, true), true);
+		request.getSession().setAttribute(Json.session.get(), session.id());
+		return session;
+	}
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response, Map<String, String> parameters, List<file> files, ISession session) throws IOException {
+		response.sendRedirect("/");
+	}
+	
+	private boolean contains(String[] strings, String value) {
+		for(String string : strings)
+			if(string.equals(value))
+				return true;
+		
+		return false;
+	}
+}
---
+++ org.zenframework.z8.servlet/src/main/java/org/zenframework/z8/web/server/RedirectAdapter.java
@@ -0,0 +1,61 @@
+package org.zenframework.z8.web.server;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.zenframework.z8.server.config.ServerConfig;
+import org.zenframework.z8.server.crypto.Crypto;
+import org.zenframework.z8.server.engine.ISession;
+import org.zenframework.z8.server.json.Json;
+import org.zenframework.z8.server.json.parser.JsonObject;
+import org.zenframework.z8.server.security.IUser;
+import org.zenframework.z8.server.types.file;
+
+public class RedirectAdapter extends Adapter {
+	static private final String AdapterPath = "/redirect";
+
+	@Override
+	public boolean canHandleRequest(HttpServletRequest request) {
+		return request.getServletPath().equals(AdapterPath);
+	}
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response, Map<String, String> parameters, List<file> files, ISession session) throws IOException {
+		String remoteDomain = parameters.get("domain");
+		String url = parameters.get("url");
+		String[] localDomains = ServerConfig.applicationServer().domains();
+		
+		if(remoteDomain == null || localDomains.length == 0 || contains(localDomains, remoteDomain)) {
+			super.service(request, response, parameters, files, session);
+			return;
+		}
+		if(url == null)
+			throw new RuntimeException("Url is null");
+		
+		IUser user = session.user();
+		JsonObject json = new JsonObject();
+		json.put(Json.domain.get(), remoteDomain);
+		json.put(Json.login.get(), user.login());
+		json.put(Json.time.get(), System.currentTimeMillis());
+		String encrypt = Crypto.Default.encrypt(json.toString());
+		String urlJson = URLEncoder.encode(encrypt, StandardCharsets.UTF_8.name());
+
+		if(response != null)
+			response.sendRedirect(url + (url.endsWith("/") ? "" : "/") + "accept?"
+									  + "json=" + urlJson);
+	}
+	
+	private boolean contains(String[] strings, String value) {
+		for(String string : strings)
+			if(string.equals(value))
+				return true;
+		
+		return false;
+	}
+}
